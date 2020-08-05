68acf931e127b8a4734615a939235eca01f34ee4
Sumtudou <1163015195@qq.com>
2020-08-05 14:15:07 +0800

    [TodoList][Jim&Jay] feature: add Layout page

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 import AllListPage from './component/AllListPage'
 import FinishedListPage from './component/FinishedListTable'
 import { HashRouter, Route } from 'react-router-dom'
-import Menu from './component/Menu'
+import RouterMenu from './component/RouterMenu'
 import AxiosPage from './component/AxiosPage'
+import 'antd/dist/antd.css'
+import { Layout } from 'antd'
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Layout>
         <HashRouter>
-          <Menu />
-          <div>
-            <Route exact path='/' component={AllListPage} />
-            <Route path='/axios' component={AxiosPage} />
-            <Route path='/finished' component={FinishedListPage} />
-          </div>
+          <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            <RouterMenu />
+
+          </Header>
+
+          <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+              <Route exact path='/' component={AllListPage} />
+              <Route path='/axios' component={AxiosPage} />
+              <Route path='/finished' component={FinishedListPage} />
+            </div>
+          </Content>
         </HashRouter>
-      </header>
+        <Footer style={{ textAlign: 'center' }}>©2020 Created by Jim&Jay</Footer>
+      </Layout>
     </div>
   );
 }
---
+++ todolist/src/component/RouterMenu/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Menu }  from 'antd'
+
+class RouterMenu extends React.Component {
+    render() {
+        return (
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                <Menu.Item key="1"><Link to="/" style={{ color: 'white' }}>All List Page</Link></Menu.Item>
+                <Menu.Item key="2"><Link to="/finished" style={{ color: 'white' }}>Finished List Page</Link></Menu.Item>
+                <Menu.Item key="3"><Link to="/axios" style={{ color: 'white' }}>Axios Page</Link></Menu.Item>
+            </Menu>
+        )
+    }
+
+}
+export default RouterMenu
